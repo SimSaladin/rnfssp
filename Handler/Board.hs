84944e466eb20643b7b3155531932989a6e0fcb7
Samuli Thomasson <simsaladin@paivola.fi>
2012-04-08 16:35:03 +0300

    random update

--- Handler/Board.hs
+++ Handler/Board.hs
@@ -4,12 +4,29 @@ import Import
 
 getBoardR :: Handler RepHtml
 getBoardR = do
+    (formWidget, formEnctype) <- generateFormPost boardForm
+    let submission = Nothing :: Maybe (Text, Text, Text)
+        handlerName = "getBoardR" :: Text
     defaultLayout $ do
         setTitle "Welcome SS/board!"
         $(widgetFile "board")
 
 postBoardR :: Handler RepHtml
 postBoardR = do
+    ((result, formWidget), formEnctype) <- runFormPost boardForm
+    let handlerName = "postHomeR" :: Text
+        submission = case result of
+            FormSuccess res -> Just res
+            _ -> Nothing
+
     defaultLayout $ do
         setTitle "Thanks for posting!"
         $(widgetFile "board")
+
+boardForm :: Form Boardpost
+boardForm = renderDivs $ Boardpost
+    <$> areq textField "Title" Nothing
+    <*> areq textField "Name" Nothing
+    <*> areq textareaField "Content" Nothing
+    <*> areq hiddenField "parent" Nothing
+
