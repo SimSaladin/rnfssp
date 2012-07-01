985a92c39bf52bc93e5d0811e42f669ddfc3c34b
Samuli Thomasson <simsaladin@paivola.fi>
2012-07-02 01:53:00 +0300

    fix registration and tweak styles

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -34,7 +34,7 @@ registerForm :: Html -> MForm App App (FormResult User, Widget)
 registerForm extra = do
     (resName, viewName) <- mreq textField "Username" Nothing
     (resComm, viewComm) <- mreq textareaField "Comment" Nothing
-    (resPass, viewPass) <- mreq passwordAndConfirmField "Password" Nothing
+    (resPass, viewPass) <- mreq passwordConfirmField "Password" Nothing
     let resUser = User <$> resName
                        <*> resPass
                        <*> pure ""
@@ -43,23 +43,25 @@ registerForm extra = do
                        <*> pure Nothing
                        <*> resComm
         in return (resUser, $(widgetFile "form-register"))
-    
-passwordAndConfirmField :: Field sub master Text
-passwordAndConfirmField = Field
-    { fieldParse = \raw -> case raw of
-        [a, b]
-            | T.length a < 4 -> return $ Left "Password must be at least 4 characters"
-            | a == b -> return $ Right $ Just a
-            | otherwise -> return $ Left "Passwords don't match"
-        _ -> return $ Left "You must enter two values"
-    , fieldView = \aId aName _ eRes isReq -> [whamlet|
+
+passwordConfirmField :: Field sub master Text
+passwordConfirmField = Field
+    { fieldParse = \rawVals ->
+        case rawVals of
+            [a, b]
+                | T.length a < 4 -> return $ Left "Password must be at least 4 characters"
+                | a == b -> return $ Right $ Just a
+                | otherwise -> return $ Left "Passwords don't match"
+            [] -> return $ Right Nothing
+            _ -> return $ Left "You must enter two values"
+    , fieldView = \idAttr nameAttr _ eResult isReq -> [whamlet|
 <div.control-group>
-  <div.control-label for=#{aId}> Password
+  <div.control-label for=#{idAttr}> Password
   <div.controls>
-    <input id=#{aId} name=#{aName} type=password :isReq:required>
+    <input id=#{idAttr} name=#{nameAttr} type=password :isReq:required>
 <div.control-group>
-  <div.control-label for=#{aId}-confirm> Confirm password
+  <div.control-label for=#{idAttr}-confirm> Confirm password
   <div.controls>
-    <input id=#{aId}-confirm name=#{aName}-confirm type=password :isReq:required>
-    |]
+    <input id=#{idAttr}-confirm name=#{nameAttr} type=password>
+|]
     }
