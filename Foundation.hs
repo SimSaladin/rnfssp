88996f695256c1ed0051f1bee0ecc69e6597c160
Samuli Thomasson <simsaladin@paivola.fi>
2012-07-15 15:21:56 +0300

    increase session timeout to 36 hours

--- Foundation.hs
+++ Foundation.hs
@@ -86,7 +86,7 @@ instance Yesod App where
     -- default session idle timeout is 120 minutes
     makeSessionBackend _ = do
         key <- getKey "config/client_session_key.aes"
-        return . Just $ clientSessionBackend key 120
+        return . Just $ clientSessionBackend key 2160
 
     defaultLayout widget = do
         master <- getYesod
