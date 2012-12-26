f10e6121e0d359c952d71bf378f12cce9c2da0e6
Samuli Thomasson <simsaladin@paivola.fi>
2012-12-26 18:24:53 +0200

    fix Configs section parameters

--- Configs.hs
+++ Configs.hs
@@ -2,7 +2,7 @@
 ------------------------------------------------------------------------------
 -- File:          Configs.hs
 -- Creation Date: Dec 24 2012 [01:31:05]
--- Last Modified: Dec 25 2012 [19:23:36]
+-- Last Modified: Dec 26 2012 [18:12:25]
 -- Created By: Samuli Thomasson [SimSaladin] samuli.thomassonAtpaivola.fi
 ------------------------------------------------------------------------------
 module Configs ( module Sections, onSec ) where
@@ -18,7 +18,7 @@ animeContent = FilmSec "anime" "/home/media/anime" (MediaContentR "anime")
 
 -- |
 musicContent :: MPDSec
-musicContent = MPDSec "music" (MediaContentR "music") "/home/media/music"
+musicContent = MPDSec "music" "/home/media/music" (MediaContentR "music")
 
 onSec :: Text -> (forall a. MSection a => a -> b) -> b
 onSec sec f = case sec of
